16e57477832dcbe516258dc996bd9883452936fa
Michelle Liu <michelleliu103@gmail.com>
2019-01-29 18:20:45 -0800

    frontend draft

---
+++ frontend/javascript.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("bpm_dropdown_content").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.bpm_button')) {
+    var dropdowns = document.getElementsByClassName("bpm_dropdown_content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
