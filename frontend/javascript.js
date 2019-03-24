c99f40941920a577657d6fda8ccd79b36aee294f
Michelle Liu <michelleliu103@gmail.com>
2019-03-24 17:10:46 +0800

    adding the mute and clear button

--- frontend/javascript.js
+++ frontend/javascript.js
@@ -1,6 +1,56 @@
 console.clear();
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
+//start of copy and paste
+var analyser = audioCtx.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+//src.connect(analyser);
+    analyser.connect(audioCtx.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i]*1.5;
+        
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+
+//end of paste
 
 //switch aria attribute on click
 const pads = document.querySelectorAll('.pads');
@@ -24,6 +74,120 @@ function clearButton(){
     })
 };
 
+const allHihatButtons = document.querySelectorAll('.drum-one .pads button');
+const allSnareButtons = document.querySelectorAll('.drum-two .pads button');
+const allKickButtons = document.querySelectorAll('.drum-three .pads button');
+
+function clearHihat(){
+	console.log("clear hihat");
+	allHihatButtons.forEach(el => {
+        el.setAttribute('aria-checked', 'false');
+    })
+}
+
+function clearSnare(){
+	console.log("clear snare");
+	allSnareButtons.forEach(el => {
+        el.setAttribute('aria-checked', 'false');
+    })
+}
+
+function clearKick(){
+	console.log("clear kick");
+	allKickButtons.forEach(el => {
+        el.setAttribute('aria-checked', 'false');
+    })
+}
+
+let mutedHihat = 0;
+let mutedSnare = 0;
+let mutedKick = 0;
+
+var unmuteHihatButton = document.getElementById("unmuteHihatButton");
+var muteHihatButton = document.getElementById("muteHihatButton");
+
+var unmuteSnareButton = document.getElementById("unmuteSnareButton");
+var muteSnareButton = document.getElementById("muteSnareButton");
+
+var unmuteKickButton = document.getElementById("unmuteKickButton");
+var muteKickButton = document.getElementById("muteKickButton");
+
+function muteHihat(){
+
+	if (mutedHihat == 0){
+	muteHihatButton.style.display = 'none';
+	unmuteHihatButton.style.display = 'inline-block';
+	
+	console.log("mute hihat");
+	mutedHihat = 1;
+	allHihatButtons.forEach(el => {
+        el.style.opacity = 0.5;
+    })
+	}
+	
+	else{
+	muteHihatButton.style.display = 'inline-block';
+	unmuteHihatButton.style.display = 'none';
+	
+	console.log("unmute hihat");
+	mutedHihat = 0;
+	allHihatButtons.forEach(el => {
+		el.style.opacity = 1;
+    })
+	}
+}
+
+
+function muteSnare(){
+
+	if (mutedSnare == 0){
+	muteSnareButton.style.display = 'none';
+	unmuteSnareButton.style.display = 'inline-block';
+	
+	console.log("mute snare");
+	mutedSnare = 1;
+	allSnareButtons.forEach(el => {
+        el.style.opacity = 0.5;
+    })
+	}
+	
+	else{
+	muteSnareButton.style.display = 'inline-block';
+	unmuteSnareButton.style.display = 'none';
+	
+	console.log("unmute snare");
+	mutedSnare = 0;
+	allSnareButtons.forEach(el => {
+		el.style.opacity = 1;
+    })
+	}
+}
+
+function muteKick(){
+
+	if (mutedKick == 0){
+	muteKickButton.style.display = 'none';
+	unmuteKickButton.style.display = 'inline-block';
+	
+	console.log("mute kick");
+	mutedKick = 1;
+	allKickButtons.forEach(el => {
+        el.style.opacity = 0.5;
+    })
+	}
+	
+	else{
+	muteKickButton.style.display = 'inline-block';
+	unmuteKickButton.style.display = 'none';
+	
+	console.log("unmute kick");
+	mutedKick = 0;
+	allKickButtons.forEach(el => {
+		el.style.opacity = 1;
+    })
+	}
+}
+
 // Loading ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // fetch the audio file and decode the data
 async function getFile(audioContext, filepath) {
@@ -42,6 +206,8 @@ function playSample(audioContext, audioBuffer) {
   sampleSource.playbackRate.setValueAtTime(playbackRate, audioCtx.currentTime);
   sampleSource.connect(audioContext.destination)
   sampleSource.start();
+	//copy
+  sampleSource.connect(analyser);
   return sampleSource;
 }
 
@@ -88,9 +254,11 @@ async function setupSample() {
   //var x = Math.floor((Math.random() * 3) + 1);
 	//console.log(x);
  // const filePath = `https://s3.amazonaws.com/astro-drum/GANKick_${x}.wav` ;
+	
 	const Hihat_filePath = "https://s3.amazonaws.com/astro-drum/hihat/gen_hihat_0.wav" ;
 	const Snare_filePath =  "https://s3.amazonaws.com/astro-drum/snare/gen_snare_0.wav" ;
 	const Kick_filePath =  "https://s3.amazonaws.com/astro-drum/gen_kick_12.wav" ;
+	
   // Here we're `await`ing the async/promise that is `getFile`.
   // To be able to use this keyword we need to be within an `async` function
   const kick_sample = await getFile(audioCtx, Kick_filePath);
@@ -174,15 +342,16 @@ function scheduleNote(beatNumber, time) {
     // push the note on the queue, even if we're not playing.
     notesInQueue.push({ note: beatNumber, time: time });
 
-    if (pads[0].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') {
+	//if the row is NOTmute and the button is pressed, then play
+    if ((pads[0].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') && (mutedHihat==0)) {
         playSample(audioCtx, hihat);
 		//console.log("hihat");
     }
-    if (pads[1].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') {
+    if ((pads[1].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') &&(mutedSnare==0)) {
         playSample(audioCtx, snare);
 		//console.log("snare");
     }
-    if (pads[2].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') {
+    if ((pads[2].querySelectorAll('button')[currentNote].getAttribute('aria-checked') === 'true') && (mutedKick==0)) {
         playSample(audioCtx, kick);
 		//console.log("kick");
     }
@@ -227,50 +396,13 @@ function draw() {
 const playButton = document.querySelector('[data-playing]');
 var pauseButton = document.getElementById("stop_button");
 
+//TODO:
 function saveButton(){
 	arrayOfAudioBuffers = [hihat,snare,kick];
 	mixed = mix(arrayOfAudioBuffers);
 	console.log(mixed);
-	
-	
 }
 
-/*
-function mix(buffers) {
-
-    var nbBuffer = buffers.length;// Get the number of buffer contained in the array buffers
-    var maxChannels = 0;// Get the maximum number of channels accros all buffers
-    var maxDuration = 0;// Get the maximum length
-
-    for (var i = 0; i < nbBuffer; i++) {
-        if (buffers[i].numberOfChannels > maxChannels) {
-            maxChannels = buffers[i].numberOfChannels;
-        }
-        if (buffers[i].duration > maxDuration) {
-            maxDuration = buffers[i].duration;
-        }
-    }
-
-    // Get the output buffer (which is an array of datas) with the right number of channels and size/duration
-    var mixed = audioCtx.createBuffer(maxChannels, audioCtx.sampleRate * maxDuration, audioCtx.sampleRate);        
-
-    for (var j=0; j<nbBuffer; j++){
-
-        // For each channel contained in a buffer...
-        for (var srcChannel = 0; srcChannel < buffers[j].numberOfChannels; srcChannel++) {
-
-            var _out = mixed.getChannelData(srcChannel);// Get the channel we will mix into
-            var _in = buffers[j].getChannelData(srcChannel);// Get the channel we want to mix in
-
-            for (var i = 0; i < _in.length; i++) {
-                _out[i] += _in[i];// Calculate the new value for each index of the buffer array
-            }
-        }
-    }
-
-    return mixed;
-}
-*/
 
 let isPlaying = false;
 setupSample()
@@ -298,6 +430,8 @@ setupSample()
         nextNoteTime = audioCtx.currentTime;
         scheduler(); // kick off scheduling
         requestAnimationFrame(draw); // start the drawing loop.
+		//copy
+		  renderFrame();
         ev.target.dataset.playing = 'true';
       } else {
         window.clearTimeout(timerID);
